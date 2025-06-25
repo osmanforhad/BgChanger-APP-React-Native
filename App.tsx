41c069730e8df3bbef4155ba6c347662a7493d36
osmanforhad <osmanforhad93@gmail.com>
2025-06-25 11:03:43 +0600

    basic project setup with sdk location

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+
+  return (
+    <View>
+      <Text>BG Changer App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
