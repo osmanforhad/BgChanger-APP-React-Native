22dcd1c75272b8693c0a349907271c2aaa67ac4b
osmanforhad <osmanforhad93@gmail.com>
2025-06-25 11:33:56 +0600

    write logic for bg changer app

--- App.tsx
+++ App.tsx
@@ -5,23 +5,61 @@
  * @format
  */
 
+import { useState } from 'react';
 import {
+  StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
 function App() {
 
+  //defining the state
+  const [randomBackground, setRandomBackground] = useState("#ffffff");
+  //define method for generate the colors
+  const generateColor = () => {
+    const hexRange = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    //update the state with color option
+    setRandomBackground(color);
+  }
+
   return (
-    <View>
-      <Text>BG Changer App</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={randomBackground} />
+      <View style={[styles.container, { backgroundColor: randomBackground }]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: '#6A1B4D',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionBtnTxt: {
+    fontSize: 24,
+    color: '#FFFFFF',
+    //textTransform: 'uppercase',
+  },
 });
 
 export default App;
